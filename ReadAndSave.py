e9677bb0056b5e2a901b7882922c7a5bf4d6a3aa
Sahil Vartak <sahilvartak8@gmail.com>
2024-08-05 10:23:39 -0500

    Changed to class-format, added overlay to prevent acidental interacting with screen, added region indicator

--- ReadAndSave.py
+++ ReadAndSave.py
@@ -1,71 +1,113 @@
 #Imports
 import pyautogui
 import pytesseract
-from pynput import keyboard, mouse
+import tkinter as tk
+import threading
+import time
+
+from tkinter import Canvas
+from pynput import keyboard
 from PIL import Image
 
 #Path to Tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-#Variable Init
-start_x = start_y = end_x = end_y = None
-capture_mode = True
+class ReadAndSave:
+    def __init__(self):
+        #Variable Init
 
-#Click and Drag Method
-def on_click(x, y, button, pressed):
-    global start_x, start_y, end_x, end_y
+        #For capturing
+        self.start_x = self.start_y = self.end_x = self.end_y = None
+        self.region = None
+        self.capture_mode = True
 
-    if button == mouse.Button.left:
-        #Start Point
-        if pressed:
-            start_x, start_y = x, y
-        #End Point
-        else:
-            end_x, end_y = x, y
+        #For overlay
+        self.overlay = None
+        self.canvas = None
+        self.root = None
 
-            region = (min(start_x, end_x), min(start_y, end_y),
-                      abs(end_x - start_x), abs(end_y - start_y))
+    def create_overlay(self):
+        #Overlay
+        self.root = tk.Tk()
+        self.root.attributes("-fullscreen", True)
+        self.root.attributes("-topmost", True)
+        #self.root.attributes("-transparentcolor", "black")
+        self.root.attributes("-alpha", "0.5")
+        self.root.config(bg="gray")
 
-            capture_and_extract(region)
+        self.canvas = Canvas(self.root, bg="black", highlightthickness=0)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        
+        self.canvas.bind("<ButtonPress-1>", self.on_mouse_down)
+        self.canvas.bind("<B1-Motion>", self.on_mouse_drag)
+        self.canvas.bind("<ButtonRelease-1>", self.on_mouse_up)
 
-            return False
+    def on_mouse_down(self, event):
+        print("Mouse down")
+        self.start_x, self.start_y = event.x, event.y
+        self.rect_id = self.canvas.create_rectangle(self.start_x, self.start_y, self.start_x, self.start_y,
+                                                    outline="red", width=2, fill="red", stipple="gray25")
+    
+    def on_mouse_drag(self, event):
+        print("Mouse drag")
+        self.end_x, self.end_y = event.x, event.y
+        self.canvas.coords(self.rect_id, self.start_x, self.start_y, self.end_x, self.end_y)
 
-#Self-explanatory
-def capture_and_extract(region):
-    #Take screenshot
-    screenshot = pyautogui.screenshot(region = region)
+    def on_mouse_up(self, event):
+        print("Mouse up")
+        self.end_x, self.end_y = event.x, event.y
+        threading.Thread(target=self.capture_and_extract, args=(self.start_x, self.start_y, self.end_x, self.end_y)).start()
+        time.sleep(0.001)
+        self.root.destroy()
 
-    #Save ss
-    screenshot_path = "r&s_screenshot.png"
-    screenshot.save(screenshot_path)
 
-    #Extract text
-    text = pytesseract.image_to_string(Image.open(screenshot_path))
+    #Self-explanatory
+    def capture_and_extract(self, start_x, start_y, end_x, end_y):
+        #Take screenshot
+        print("Capture start")
+        region = (min(start_x, end_x), min(start_y, end_y), 
+                       abs(end_x - start_x), abs(end_y - start_y))
 
-    #Save text
-    with open("extracted_text.txt", "a") as file:
-        file.write(text + "\n")
+        screenshot = pyautogui.screenshot(region = region)
 
-    print("Extracted and saved.")
+        #Save ss
+        screenshot_path = "r&s_screenshot.png"
+        screenshot.save(screenshot_path)
 
-#Excute C&A 
-def on_activate_capture():
-    print("Click and Drag...")
-    mouse_listener = mouse.Listener(on_click = on_click)
-    mouse_listener.start()
+        #Extract text
+        text = pytesseract.image_to_string(Image.open(screenshot_path))
 
-#Exit
-def on_activate_quit():
-    global capture_mode
-    print("Quitting...")
-    capture_mode = False
+        #Save text
+        with open("extracted_text.txt", "a") as file:
+            file.write("------------------------------------------\n\n")
+            file.write(text + "\n")
+
+        print("Extracted and saved.")
+
+    #Excute C&A
+    def on_activate_capture(self):
+        print("Click and Drag...")
+        self.create_overlay()
+        self.root.mainloop()
+
+    #Exit
+    def on_activate_quit(self):
+        print("Quitting...")
+        self.capture_mode = False
     
 
 #Main
-listener = keyboard.GlobalHotKeys({
-    '<ctrl>+<alt>+s': on_activate_capture,
-    '<ctrl>+<alt>+q': on_activate_quit})
-listener.start()
+def main():
+    print("Start!")
+    read_and_save = ReadAndSave()
 
-while capture_mode:
-    pass
+    listener = keyboard.GlobalHotKeys({
+        '<ctrl>+<alt>+s': read_and_save.on_activate_capture,
+        '<ctrl>+<alt>+q': read_and_save.on_activate_quit})
+    listener.start()
+
+    while read_and_save.capture_mode:
+        pass
+
+if __name__ == "__main__":
+    main()
